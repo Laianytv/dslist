0baaf5015429fa688ec142e356db1d0f1021ac9c
Laiany vieira <laianyleite56@gmail.com>
2023-05-15 11:46:43 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -22,9 +22,14 @@ import jakarta.persistence.Table;
 	@Column(name = "game_year")
 	private  Integer year;
 	private  String  genre;
-	private  String  platform;
+	private  String  platforms;
+	private  Double  score;
 	private  String  imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private  String  shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private  String  longDescription;
 	
 	
@@ -33,14 +38,15 @@ import jakarta.persistence.Table;
 	}
 
 
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double  score , String imgUrl,
 			String shortDescription, String longDescription) {
 		super();
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -77,13 +83,34 @@ import jakarta.persistence.Table;
 	}
 
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatform(String platforms) {
+		this.platforms = platforms;
+	}
+    
+	
+
+	public Integer getYear() {
+		return year;
+	}
+
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+
+	public Double getScore() {
+		return score;
+	}
+
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 
